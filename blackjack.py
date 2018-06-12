030c9dedc2a60928c1415636e25232244e597605
Holly Grimm <holly.grimm@gmail.com>
2018-06-12 14:41:54 -0600

    rename state to observation

--- blackjack.py
+++ blackjack.py
@@ -15,47 +15,47 @@ class BlackjackAgent(object):
         # track episode num for episilon
         self.i_episode = 0
 
-    def log_state(self, state):
-        player_hand_sum, dealer_showing_card, usable_ace = zip(state)
+    def log_observation(self, observation):
+        player_hand_sum, dealer_showing_card, usable_ace = zip(observation)
         logger.info('players current sum:{}, dealer showing card:{}, usable ace:{}'.format(player_hand_sum[0], dealer_showing_card[0], usable_ace[0]))
 
-    def log_done(self, state, reward):
-        self.log_state(state)
+    def log_done(self, observation, reward):
+        self.log_observation(observation)
         logger.info('final reward:{}'.format(reward))
 
-    def get_policy_for_state(self, Q_s, epsilon):
+    def get_policy_for_observation(self, Q_s, epsilon):
         """ calculates the action probabilities corresponding to epsilon-greedy policy """
         policy_s = np.ones(self.nA) * epsilon / self.nA
         best_a = np.argmax(Q_s)
         policy_s[best_a] = 1 - epsilon + (epsilon / self.nA)
         return policy_s
 
-    def choose_action_by_policy(self, policy, state):
+    def choose_action_by_policy(self, policy, observation):
         """ selects action based on trained policy """
-        if state in policy:
-            action = policy[state]
+        if observation in policy:
+            action = policy[observation]
         else:
-            # state not found in policy, usually if there hasn't been enough training episodes
+            # observation not found in policy, usually if there hasn't been enough training episodes
             action = self.action_space.sample()
         return action
 
-    def choose_action(self, state, epsilon):
-        """ if state in Q dict, choose random action with probability of eps otherwise sample from action space """
-        if state in self.Q:
-            action = np.random.choice(np.arange(self.nA), p=self.get_policy_for_state(self.Q[state], epsilon))
+    def choose_action(self, observation, epsilon):
+        """ if observation in Q dict, choose random action with probability of eps otherwise sample from action space """
+        if observation in self.Q:
+            action = np.random.choice(np.arange(self.nA), p=self.get_policy_for_observation(self.Q[observation], epsilon))
         else:
             action = self.action_space.sample()
         return action
 
     def update_action_val_function(self, episode):
         """ updates the action-value function Q and N count dictionaries for one episode """
-        states, actions, rewards = zip(*episode)
+        observations, actions, rewards = zip(*episode)
         discounts = np.array([self.GAMMA**i for i in range(len(rewards)+1)])
-        for i, state in enumerate(states):
-            old_Q = self.Q[state][actions[i]] 
-            old_N = self.N[state][actions[i]]
-            self.Q[state][actions[i]] = old_Q + (sum(rewards[i:]*discounts[:-(1+i)]) - old_Q)/(old_N+1)
-            self.N[state][actions[i]] += 1
+        for i, observation in enumerate(observations):
+            old_Q = self.Q[observation][actions[i]] 
+            old_N = self.N[observation][actions[i]]
+            self.Q[observation][actions[i]] = old_Q + (sum(rewards[i:]*discounts[:-(1+i)]) - old_Q)/(old_N+1)
+            self.N[observation][actions[i]] += 1
 
     def generate_episode(self, env):
         """ start new episode and update action-value function until episode terminates """
@@ -63,35 +63,35 @@ class BlackjackAgent(object):
         # decay epsilon
         epsilon = 1.0/((self.i_episode/8000) + 1)
         episode = []
-        state = env.reset()
-        self.log_state(state)
+        observation = env.reset()
+        self.log_observation(observation)
         while True:
-            action = self.choose_action(state, epsilon)
+            action = self.choose_action(observation, epsilon)
             logger.info('HIT' if action else 'STICK')
-            next_state, reward, done, _ = env.step(action)
-            episode.append((state, action, reward))
+            next_observation, reward, done, _ = env.step(action)
+            episode.append((observation, action, reward))
             if done:
-                self.log_done(next_state, reward)
+                self.log_done(next_observation, reward)
                 break
             else:
-                self.log_state(next_state)
-            state = next_state
+                self.log_observation(next_observation)
+            observation = next_observation
         return episode
 
     def score(self, env, policy, num_episodes=1000):
         """ average score using policy after num_episodes """
         rewards = []
         for _ in range(num_episodes):
-            state = env.reset()
+            observation = env.reset()
             rewards_sum = 0
             while True:
-                action = self.choose_action_by_policy(policy, state)
-                next_state, reward, done, _ = env.step(action)
+                action = self.choose_action_by_policy(policy, observation)
+                next_observation, reward, done, _ = env.step(action)
                 rewards_sum += reward
                 if done:
                     rewards.append(rewards_sum)
                     break
-                state = next_state
+                observation = next_observation
         return np.mean(rewards)
 
 
